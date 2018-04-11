dddacbbcc277ad04e143601d0554da302bd042f2
Sonu Sharma <sonu.sharma045@gmail.com>
2018-04-11 15:57:22 +0530

    Css and javascript files added to timepicker

---
+++ Events-Lab/Events.Web/fonts/Models/EventViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Events.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace Events.Web.Models
+{
+    public class EventViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime StartDateTIme { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+
+     //   public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public string Location { get; set; }
+
+        //      public bool IsPublic { get; set; }
+
+        public static Expression<Func<Event, EventViewModel>> viewModel
+        {
+            get
+            {
+                return e => new EventViewModel()
+                {
+
+                    Id = e.Id,
+                    Title = e.Title,
+                    StartDateTIme = e.StartDateTIme,
+                    Duration = e.Duration,
+                    Location = e.Location,
+                    Author = e.Author.Fullname
+                };
+
+            }
+
+        }
+    }
+}
---
+++ Events-Lab/Events.Web/fonts/Models/UpcomingPassedEventsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Events.Web.Models
+{
+    public class UpcomingPassedEventsViewModel
+    {
+        public IEnumerable<EventViewModel> UpcomingEvents { get; set; }
+        public IEnumerable<EventViewModel> PassedEvents { get; set; }
+    }
+}
